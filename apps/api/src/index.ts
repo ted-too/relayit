4ea5a0ac4e75c9ea8af257f9553bba77a0f96061
Ted Too <tedtoo6@outlook.com>
2025-04-28 13:37:24 +1000

    init

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default {
+  port: process.env.PORT || 3000,
+  fetch: app.fetch,
+};
